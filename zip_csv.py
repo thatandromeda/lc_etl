b77ac97c2bf9ff2116ba42f0b9daca666b9ebb3c
Andromeda Yelton <andromeda.yelton@gmail.com>
2021-10-01 10:39:30 -0400

    use correct data structure

--- zip_csv.py
+++ zip_csv.py
@@ -3,7 +3,7 @@ import json
 import logging
 from pathlib import Path
 
-from fetch_metadata import METADATA_ORDER, OUTPUT_DIR
+from fetch_metadata import METADATA_ORDER, OUTPUT_DIR, is_chronam, identifier_from_chronam
 from queries import make_timestamp
 
 Path(OUTPUT_DIR).mkdir(exist_ok=True)
@@ -12,6 +12,12 @@ logging.basicConfig(filename=f'{OUTPUT_DIR}/zip_{make_timestamp()}.log',
                     level=logging.INFO)
 
 header = ['x', 'y'] + METADATA_ORDER
+
+def extract_dict(identifier, metadata):
+    if is_chronam(identifier):
+        return metadata[identifier_from_chronam(identifier)]
+
+    return metadata[identifier]
 
 with open('viz/labeled_model.csv', 'w', newline='') as outfile:
     csv_output = csv.writer(outfile, delimiter=',')
@@ -25,12 +31,14 @@ with open('viz/labeled_model.csv', 'w', newline='') as outfile:
             try:
                 with open(OUTPUT_DIR / Path(identifier.strip())) as f:
                     raw_item_metadata = json.load(f)
-                raw_item_metadata = [str(x) for x in list(raw_item_metadata.values())]
+
+                item_metadata = extract_dict(identifier, raw_item_metadata)
+                item_metadata = [item_metadata[key] for key in METADATA_ORDER]
             except (KeyError, json.JSONDecodeError) as e:
                 # Sometimes we didn't successfully fetch the metadata.
                 logging.exception(f"Couldn't zip metadata for {identifier}")
                 continue
 
-            item_metadata = coordinate.strip().split(',') + raw_item_metadata
+            item_metadata = coordinate.strip().split(',') + item_metadata
 
             csv_output.writerow(item_metadata)
