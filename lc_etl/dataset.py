7275aa1044c25e7ba3aaf758abb35b3ea6951cc0
Andromeda Yelton <andromeda.yelton@gmail.com>
2021-12-17 10:35:51 -0500

    Minor bugfixes

--- lc_etl/dataset.py
+++ lc_etl/dataset.py
@@ -28,36 +28,39 @@ if __name__ == '__main__':
         fromlist=['collections', 'date_filtered_collections', 'items', 'queries', 'newspapers']
     )
 
-    if data_def.collections:
+    try:
         logging.info('Fetching data from collections...')
         slurp_collections(data_def.collections)
-    else:
+    except AttributeError:
         logging.info('No collections defined.')
 
-    if data_def.date_filtered_collections:
+    try:
         logging.info('Fetching data from date-filtered collections...')
         slurp_collections(data_def.date_filtered_collections, filter_for_dates=True)
-    else:
+    except AttributeError:
         logging.info('No date-filtered collections defined.')
 
-    if data_def.queries:
+    try:
         logging.info('Fetching data from queries...')
         for query in data_def.queries:
             logging.info(query)
             slurp(url=query)
-    else:
+    except AttributeError:
         logging.info('No queries defined.')
 
-    if data_def.items:
+    try:
         logging.info('Fetching data from items...')
         slurp_items(data_def.items)
-    else:
+    except AttributeError:
         logging.info('No items defined.')
 
-    logging.info('Fetching data from ChronAm...')
-    if data_def.newspapers.get('goal_dates'):
-        slurp_newspapers(goal_dates=data_def.newspapers['goal_dates'])
-    elif data_def.newspapers.get('count'):
-        slurp_newspapers(count=data_def.newspapers['count'])
-    else:
-        slurp_newspapers()
+    try:
+        logging.info('Fetching data from ChronAm...')
+        if data_def.newspapers.get('goal_dates'):
+            slurp_newspapers(goal_dates=data_def.newspapers['goal_dates'])
+        elif data_def.newspapers.get('count'):
+            slurp_newspapers(count=data_def.newspapers['count'])
+        else:
+            slurp_newspapers()
+    except AttributeError:
+        logging.info('No newspapers defined')
